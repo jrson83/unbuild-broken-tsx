2044c3d0e231dbbbd7123f06ff182aa00dbffe1a
jrson83 <me@jrson.de>
2022-12-31 02:16:56 +0100

    first commit

---
+++ build.config.ts
@@ -0,0 +1,15 @@
+import { defineBuildConfig } from 'unbuild';
+
+export default defineBuildConfig({
+  entries: ['src/index'],
+  externals: ['react', 'react-dom'],
+  clean: true,
+  declaration: true,
+  rollup: {
+    emitCJS: true,
+    inlineDependencies: true,
+    resolve: {
+      extensions: ['.ts', '.tsx'],
+    },
+  },
+});
---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import React, { ComponentType } from 'react';
+import { createRoot } from 'react-dom/client';
+
+export const createStory = <P extends object>(
+  Component: ComponentType<P>,
+  props: P
+) => {
+  return (rootElement: HTMLElement) => {
+    const root = createRoot(rootElement);
+    root.render(<Component {...props} />);
+  };
+};
